321d2efebfa72e7f9277f50f5a4a2e7255e73dee
Mohammadreza Kashani <kashani.mohammadreza54@gmail.com>
2025-12-30 15:20:32 +0330

    upade: fix task's menu , create task counter and create tasks

--- src/index.js
+++ src/index.js
@@ -89,5 +89,147 @@ AddTags.addEventListener("click" , () => {
 CloseTagsBtn.addEventListener("click" , () => {
   CloseTags.classList.add('opacity-0','-translate-y-4','pointer-events-none','hidden')
   AddTags.classList.remove('hidden')
-  DisplayTg.classList.add('hidden')
 })
+// disable add button
+const Taskname = document.querySelector("#task-name")
+const Taskinfo = document.querySelector("#task-info")
+const Button = document.querySelector("#btn-dsble")
+
+function checkinputs() {
+  Button.disabled =
+  Taskname.value.trim() === "" || Taskinfo.value.trim() === "" || SelectedTag === null ;
+}
+
+Taskname.addEventListener("input" , checkinputs);
+Taskinfo.addEventListener("input" , checkinputs);
+
+// selected tag
+let SelectedTag = null;
+let BorderTag = null;
+
+const RedTag = document.querySelector("#red-tag")
+const YellowTag = document.querySelector("#ylw-tag")
+const GreenTag = document.querySelector("#green-tag")
+const AddRed = document.querySelector("#add-red")
+const AddYellow = document.querySelector("#add-ylw")
+const AddGreen = document.querySelector("#add-green")
+
+RedTag.addEventListener("click" , () => {
+CloseTags.classList.add('hidden')
+AddRed.classList.remove('hidden')
+AddRed.classList.add('flex')
+SelectedTag = {text: "بالا" , class : "bg-red-300 text-red-500"}
+BorderTag = {class :"border-r-4 border-red-500"}
+checkinputs()
+})
+YellowTag.addEventListener("click" , () => {
+CloseTags.classList.add('hidden')
+AddYellow.classList.remove('hidden')
+AddYellow.classList.add('flex')
+SelectedTag = {text: "متوسط" , class : "bg-yellow-200 text-yellow-500"}
+BorderTag = {class :"border-r-4 border-yellow-500"}
+checkinputs()
+})
+GreenTag.addEventListener("click" , () => {
+CloseTags.classList.add('hidden')
+AddGreen.classList.remove('hidden')
+AddGreen.classList.add('flex')
+SelectedTag = {text: "پایین" , class : "bg-green-200 text-green-500"}
+BorderTag = {class :"border-r-4 border-green-500"}
+checkinputs()
+})
+
+AddRed.addEventListener("click" , () => {
+  CloseTags.classList.remove('hidden')
+  AddRed.classList.add('hidden')
+})
+
+AddYellow.addEventListener("click" , () => {
+  CloseTags.classList.remove('hidden')
+  AddYellow.classList.add('hidden')
+})
+
+AddGreen.addEventListener("click" , () => {
+  CloseTags.classList.remove('hidden')
+  AddGreen.classList.add('hidden')
+})
+// Add task list + count task + reset menu
+
+// Count tasks
+const TaskCounter = document.querySelector("#task-counter")
+function Counter(count) {
+  if (count === 0) {
+    TaskCounter.textContent = "تسکی برای امروز نداری!"
+  } else {
+    TaskCounter.textContent = `${count} تسک را باید انجام دهید`
+  }
+}
+
+const TaskList = document.querySelector("#task-list")
+// Create tasks
+document.addEventListener("DOMContentLoaded" , () => {
+  let WriteTask = []
+  function renderlist(list) {
+    TaskList.innerHTML = list.map((task) =>
+      `<li class="relative bg-white border border-gray-300 rounded-lg shadow-sm">
+        <div class = "absolute ${task.bodertags.class} -right-10 rounded-l-lg">
+        </div>
+        <button class = "absolute top-3 left-5"><img class = "cursor-pointer" src="../assets/icons/Frame 33317.svg" alt="icon"></button>
+        <div class="lg:flex lg:flex-row">
+          <div class="flex justify-start items-center gap-5 m-3!">
+             <input class="size-5" type="checkbox" name="check">
+             <strong class="text-xl">${task.title}</strong>
+          </div>
+          <span class="inline-block mr-10! mb-3! mt-1! px-3 py-1 lg:mr-5! lg:mt-4! max-w-full rounded-lg text-sm ${task.tag.class}"> ${task.tag.text} </span>
+          </div>
+          <p class="text-gray-500 m-3! max-w-full text-ellipsis">${task.paragraph}</p>
+        </li>`
+  ).join("")
+  }
+  renderlist(WriteTask);
+  
+  
+  Button.addEventListener("click" , () => {
+    const TaskTitle = Taskname.value
+    const TaskInformation = Taskinfo.value
+    
+    const newlist = {
+      id: new Date().toISOString()
+     , title : TaskTitle
+     , paragraph : TaskInformation
+     , tag : SelectedTag
+      , bodertags : BorderTag
+    } 
+
+    //reset task's menu
+  function resetAddTaskMenu() {
+  
+  Taskname.value = "";
+  Taskinfo.value = "";
+  Button.disabled = true;
+
+  SelectedTag = null;
+  BorderTag = null;
+
+  AddRed.classList.add("hidden");
+  AddYellow.classList.add("hidden");
+  AddGreen.classList.add("hidden");
+  CloseTags.classList.add("hidden");
+  AddTags.classList.remove("hidden");
+}
+    
+    TaskList.before(AddTasks)
+    WriteTask = [...WriteTask , newlist]
+    renderlist(WriteTask)
+    Counter(WriteTask.length)
+    resetAddTaskMenu()
+
+  //reset menu page and show task list
+  AddTasks.classList.add('opacity-0','-translate-y-4','pointer-events-none','hidden')
+  AddTasks.classList.remove('opacity-100','translate-y-0','flex')
+  NoTask.classList.add('hidden')
+  OpenTaskMenu.classList.remove('hidden')
+  Taskname.addEventListener("input" , checkinputs);
+  Taskinfo.addEventListener("input" , checkinputs);
+ })
+});
